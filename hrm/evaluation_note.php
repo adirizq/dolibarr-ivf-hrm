9dd674108817de399b4536a70f10d8e50f8d5bc8
Rizky Adi <72495089+adirizq@users.noreply.github.com>
2023-10-12 11:19:14 +0700

    Show skill gap
    Create table for showing skill tab on evaluation note

--- hrm/evaluation_note.php
+++ hrm/evaluation_note.php
@@ -31,6 +31,7 @@ require '../main.inc.php';
 require_once DOL_DOCUMENT_ROOT . '/hrm/class/evaluation.class.php';
 require_once DOL_DOCUMENT_ROOT . '/hrm/class/job.class.php';
 require_once DOL_DOCUMENT_ROOT . '/hrm/lib/hrm_evaluation.lib.php';
+require_once DOL_DOCUMENT_ROOT . '/hrm/class/skill.class.php';
 
 
 // Load translation files required by the page
@@ -92,8 +93,16 @@ if (empty($reshook)) {
 $form = new Form($db);
 
 //$help_url='EN:Customers_Orders|FR:Commandes_Clients|ES:Pedidos de clientes';
+
+// Original
+// $help_url = '';
+// llxHeader('', $langs->trans('Evaluation'), $help_url);
+
+// Custom
 $help_url = '';
-llxHeader('', $langs->trans('Evaluation'), $help_url);
+$css = array();
+$css[] = '/hrm/css/style.css';
+llxHeader('', $langs->trans('Evaluation'), $help_url, '', 0, 0, '', $css);
 
 if ($id > 0 || !empty($ref)) {
 	$object->fetch_thirdparty();
@@ -126,6 +135,99 @@ if ($id > 0 || !empty($ref)) {
 
 	$cssclass = "titlefield";
 	include DOL_DOCUMENT_ROOT.'/core/tpl/notes.tpl.php';
+
+	// Custom code for showing skill gap
+	print '<h2 style="margin-top: 2rem; color: var(--colortexttitlenotab);">Skills gap</h2>';
+	if ($object->status != Evaluation::STATUS_DRAFT) {
+		// Recovery of skills related to this evaluation
+
+		$sql = 'select';
+		$sql .= '  e.ref,';
+		$sql .= '  e.date_creation,';
+		$sql .= '  e.fk_job,';
+		$sql .= '  j.label as "refjob",';
+		$sql .= '  ed.fk_skill,';
+
+		$sql .= '  sk.label as "skilllabel",';
+		$sql .= '  sk.skill_type,';
+		$sql .= '  sk.description,';
+		$sql .= '  ed.rankorder,';
+		$sql .= '  ed.required_rank,';
+		$sql .= '  ed.rankorder as "userRankForSkill",';
+		$sql .= '  skdet_user.description as "userRankForSkillDesc",';
+		$sql .= '  skdet_required.description as "required_rank_desc"';
+
+		$sql .= '  FROM ' . MAIN_DB_PREFIX . 'hrm_evaluation as e';
+		$sql .= '  LEFT JOIN ' . MAIN_DB_PREFIX . 'hrm_evaluationdet as ed ON  e.rowid = ed.fk_evaluation';
+		$sql .= '  LEFT JOIN ' . MAIN_DB_PREFIX . 'hrm_job as j ON e.fk_job = j.rowid';
+		$sql .= '  LEFT JOIN ' . MAIN_DB_PREFIX . 'hrm_skill as sk ON ed.fk_skill = sk.rowid';
+		$sql .= '  INNER JOIN ' . MAIN_DB_PREFIX . 'hrm_skilldet as skdet_user ON (skdet_user.fk_skill = sk.rowid AND skdet_user.rankorder = ed.rankorder)';
+		//$sql .= "  LEFT JOIN " . MAIN_DB_PREFIX . "hrm_skillrank as skr ON (j.rowid = skr.fk_object AND skr.fk_skill = ed.fk_skill AND skr.objecttype = 'job')";
+		$sql .= '  LEFT JOIN ' . MAIN_DB_PREFIX . 'hrm_skilldet as skdet_required ON (skdet_required.fk_skill = sk.rowid AND skdet_required.rankorder = ed.required_rank)';
+		$sql .= " WHERE e.rowid =" . ((int) $object->id);
+
+		//      echo $sql;
+
+		$resql = $db->query($sql);
+		$Tab = array();
+
+		if ($resql) {
+			$num = 0;
+			while ($obj = $db->fetch_object($resql)) {
+				if ($obj->userRankForSkill < $obj->required_rank) {
+					$Tab[$num] = new stdClass();
+					$class = '';
+					$Tab[$num]->skill_type = $obj->skill_type;
+					$Tab[$num]->skill_id = $obj->fk_skill;
+					$Tab[$num]->skilllabel = $obj->skilllabel;
+					$Tab[$num]->description = $obj->description;
+					$Tab[$num]->userRankForSkill = '<span title="' . $obj->userRankForSkillDesc . '" class="radio_js_bloc_number TNote_1">' . $obj->userRankForSkill . '</span>';
+					$Tab[$num]->required_rank = '<span title="' . $obj->required_rank_desc . '" class="radio_js_bloc_number TNote_1">' . $obj->required_rank . '</span>';
+
+					$title = $langs->trans('MaxLevelLowerThanShort');
+					$class .= 'fail diffnote-custom';
+					$content = $obj->userRankForSkill - $obj->required_rank;
+
+					$Tab[$num]->result = '<span title="' . $title . '" class="classfortooltip ' . $class . ' note">' . $content . '</span>';
+
+					$num++;
+				}
+			}
+
+			print '<div class="underbanner clearboth"></div>';
+			print '<table class="noborder centpercent">';
+
+			print '<tr class="liste_titre">';
+			print '<th style="width:auto;text-align:auto" class="liste_titre">' . $langs->trans("TypeSkill") . ' </th>';
+			print '<th style="width:auto;text-align:auto" class="liste_titre">' . $langs->trans("Label") . '</th>';
+			print '<th style="width:auto;text-align:auto" class="liste_titre">' . $langs->trans("Description") . '</th>';
+			print '<th style="width:auto;text-align:auto" class="liste_titre">Gap</th>';
+			print '</tr>';
+
+			$sk = new Skill($db);
+			foreach ($Tab as $t) {
+				$sk->fetch($t->skill_id);
+				print '<tr>';
+				print ' <td>' . Skill::typeCodeToLabel($t->skill_type) . '</td>';
+				print ' <td>' . $sk->getNomUrl(1) . '</td>';
+				print ' <td>' . $t->description . '</td>';
+				print ' <td>' . $t->result . '</td>';
+				print '</tr>';
+			}
+
+			print '</table>';
+
+			?>
+
+			<script>
+				$(document).ready(function() {
+					$(".radio_js_bloc_number").tooltip();
+				});
+			</script>
+
+			<?php
+		}
+	}
 
 	print '</div>';
 
